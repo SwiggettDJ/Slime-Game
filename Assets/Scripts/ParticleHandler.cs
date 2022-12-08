8bd16e32161e439c04b7d9242439723ea4769003
DJ <swiggettdj@gmail.com>
2022-12-08 00:49:24 -0700

    Made 2 PowerUps

--- Assets/Scripts/ParticleHandler.cs
+++ Assets/Scripts/ParticleHandler.cs
@@ -20,8 +20,7 @@ public class ParticleHandler : MonoBehaviour
     {
         List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
         int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-
-        // iterate through the particles which entered the trigger and make them red
+        
         for (int i = 0; i < numEnter; i++)
         {
             ParticleTriggerEvent.Invoke();
